def2fd476b0f04986195941c91e59516bc9fd9d2
Samuel Ribeiro <49594622+samuelrrs@users.noreply.github.com>
2019-09-25 20:35:19 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Samuel Ribeiro',
-    email: 'samuel@gmail.com',
-    password_hash: 'masterkey',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
